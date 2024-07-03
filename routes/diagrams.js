f2834b00d4af5db07df505685642a37bcb8ce2c8
Whazt <walddt05@gmail.com>
2024-07-03 12:34:16 -0600

    a

--- routes/diagrams.js
+++ routes/diagrams.js
@@ -10,8 +10,8 @@ router.get('/', async (req, res) => {
 
 // Crear un nuevo diagrama
 router.post('/', async (req, res) => {
-  const { name, productId, components, demand } = req.body; // Asegúrate de incluir name aquí
-  const newDiagram = new Diagram({ name, productId, components, demand }); // Asegúrate de pasar name al constructor
+  const { name, productId, components, demand } = req.body;
+  const newDiagram = new Diagram({ name, productId, components, demand });
   try {
     await newDiagram.save();
     res.status(201).json({ message: 'Diagrama guardado' });
@@ -26,6 +26,18 @@ router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const diagram = await Diagram.findById(id).populate('productId').populate('components.componentId');
   res.json(diagram);
+});
+
+// Eliminar un diagrama por ID
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Diagram.findByIdAndDelete(id);
+    res.status(200).json({ message: 'Diagrama eliminado' });
+  } catch (error) {
+    console.error('Error al eliminar el diagrama:', error);
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
